b668a5b044065f5efb11284895f79245c80d609e
rohitkambojjs <techliegelord@gmail.com>
2024-03-02 09:43:21 +0530

    API response and error handling part 1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 console.log("Jay Shri Ram")
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CROS_ORIDIN
+}));
+
+app.use(express.json({limit: "25kb"}));
+app.use(express.urlencoded({extends: true, limit: "25kb"}));
+app.use(express.static("public"))
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 console.log("Jay Shri Ram")
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 dotenv.config({
   path: './env'
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Server is running at port: ${process.env.PORT}`);
+  })
+  app.on("error", (error)=>{
+    console.log("ERROR:", error);
+    throw error
+  })
+})
+.catch((error)=>{
+  console.log("MongoDB connection failed !!! ", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 import express from 'express';
